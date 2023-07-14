9231c63087778e4b0cba377c3d3d52f399ae5f53
Syrone <cazonove@gmail.com>
2023-07-14 14:53:38 +0300

    added card in page category

--- app/js/app.js
+++ app/js/app.js
@@ -8,55 +8,88 @@ document.addEventListener("DOMContentLoaded", () => {
       ".price-input-slider .price-input-progress"
     );
 
-  let priceGap =
-    (document.querySelector(".range-max").getAttribute("max") * 7) / 100;
+  if (rangeInput && priceInput && progress) {
+    let priceGap =
+      (document.querySelector(".range-max").getAttribute("max") * 7) / 100;
 
-  rangeInput.forEach((input) => {
-    input.addEventListener("input", (e) => {
-      let minValue = parseInt(rangeInput[0].value),
-        maxValue = parseInt(rangeInput[1].value);
+    rangeInput.forEach((input) => {
+      input.addEventListener("input", (e) => {
+        let minValue = parseInt(rangeInput[0].value),
+          maxValue = parseInt(rangeInput[1].value);
 
-      if (maxValue - minValue < priceGap) {
-        if (e.target.className === "range-min") {
-          rangeInput[0].value = maxValue - priceGap;
+        if (maxValue - minValue < priceGap) {
+          if (e.target.className === "range-min") {
+            rangeInput[0].value = maxValue - priceGap;
+          } else {
+            rangeInput[1].value = minValue + priceGap;
+          }
         } else {
-          rangeInput[1].value = minValue + priceGap;
-        }
-      } else {
-        priceInput[0].value = minValue;
-        priceInput[1].value = maxValue;
-        progress.style.left = (minValue / rangeInput[0].max) * 100 + "%";
-        progress.style.right = 100 - (maxValue / rangeInput[1].max) * 100 + "%";
-      }
-
-      document.getElementById("btnFilterReset").addEventListener("click", () => {
-        progress.style.left = 0
-        progress.style.right = 0
-      });
-    });
-  });
-
-  priceInput.forEach((input) => {
-    input.addEventListener("input", (e) => {
-      let minValue = parseInt(priceInput[0].value),
-        maxValue = parseInt(priceInput[1].value);
-
-      if (
-        maxValue - minValue >= priceGap &&
-        maxValue <= document.querySelector(".range-max").getAttribute("max")
-      ) {
-        if (e.target.className === "input-min") {
-          rangeInput[0].value = minValue;
+          priceInput[0].value = minValue;
+          priceInput[1].value = maxValue;
           progress.style.left = (minValue / rangeInput[0].max) * 100 + "%";
-        } else {
-          rangeInput[1].value = maxValue;
           progress.style.right =
             100 - (maxValue / rangeInput[1].max) * 100 + "%";
         }
-      }
+
+        document
+          .getElementById("btnFilterReset")
+          .addEventListener("click", () => {
+            progress.style.left = 0;
+            progress.style.right = 0;
+          });
+      });
     });
-  });
+
+    priceInput.forEach((input) => {
+      input.addEventListener("input", (e) => {
+        let minValue = parseInt(priceInput[0].value),
+          maxValue = parseInt(priceInput[1].value);
+
+        if (
+          maxValue - minValue >= priceGap &&
+          maxValue <= document.querySelector(".range-max").getAttribute("max")
+        ) {
+          if (e.target.className === "input-min") {
+            rangeInput[0].value = minValue;
+            progress.style.left = (minValue / rangeInput[0].max) * 100 + "%";
+          } else {
+            rangeInput[1].value = maxValue;
+            progress.style.right =
+              100 - (maxValue / rangeInput[1].max) * 100 + "%";
+          }
+        }
+      });
+    });
+  }
   /** (End) Price Range */
+
+  /** (Start) Input Stepper */
+  const inputSteppers = document.querySelectorAll(".input-stepper");
+
+  if(inputSteppers) {
+    inputSteppers.forEach((inputStepper) => {
+      const decrementButton = inputStepper.querySelector(".decrement");
+      const incrementButton = inputStepper.querySelector(".increment");
+      const inputDisplay = inputStepper.querySelector(".input-display");
+  
+      decrementButton.addEventListener("click", () => {
+        let value = parseInt(inputDisplay.value);
+        if (value > parseInt(inputDisplay.min)) {
+          value -= parseInt(inputDisplay.step);
+          inputDisplay.value = value;
+        }
+      });
+  
+      incrementButton.addEventListener("click", () => {
+        let value = parseInt(inputDisplay.value);
+        if (value < parseInt(inputDisplay.max)) {
+          value += parseInt(inputDisplay.step);
+          inputDisplay.value = value;
+        }
+      });
+    });
+  }
+  /** (End) Input Stepper */
 
   /* (Start) Header Catalog Menu*/
   const menuCatalog = document.getElementById("catalogMenu");
